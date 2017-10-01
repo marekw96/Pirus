6d87808b5c34ca16c748e4c1d835fa471d6e2add
Pan-Marek <boteq6@o2.pl>
2017-10-01 14:24:57 +0200

    test project

---
+++ Pirus/Header/Node.h
@@ -0,0 +1,9 @@
+#pragma once
+
+namespace Pirus
+{
+	class Node
+	{
+
+	};
+}
---
+++ Tests/Tests/node-test.cpp
@@ -0,0 +1,10 @@
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
+
+#include "Header\Node.h"
+
+TEST_CASE("NODE c-tor", "[node]")
+{
+	Pirus::Node test_node;
+}
+
